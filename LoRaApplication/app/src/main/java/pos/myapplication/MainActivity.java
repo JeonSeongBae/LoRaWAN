7841bed30d3c8a4a2fac21f2635578f93aa63395
JeonSeongBae <tjdqo6042@naver.com>
2018-01-18 13:04:57 -0500

    simplify Main

--- LoRaApplication/app/src/main/java/pos/myapplication/MainActivity.java
+++ LoRaApplication/app/src/main/java/pos/myapplication/MainActivity.java
@@ -1,17 +1,16 @@
 package pos.myapplication;
 
-import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
+    import android.content.Context;
+    import android.support.v7.app.AppCompatActivity;
+    import android.os.Bundle;
+    import android.util.Log;
+    import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+    import com.google.firebase.database.DataSnapshot;
+    import com.google.firebase.database.DatabaseError;
+    import com.google.firebase.database.DatabaseReference;
+    import com.google.firebase.database.FirebaseDatabase;
+    import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
