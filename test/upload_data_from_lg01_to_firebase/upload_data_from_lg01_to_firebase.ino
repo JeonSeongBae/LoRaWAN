d9734cddbcd0e565375592c968433592f503482f
JeonSeongBae <tjdqo6042@naver.com>
2018-02-05 18:13:49 -0500

    update data to firebase complted

--- test/upload_data_from_lg01_to_firebase/upload_data_from_lg01_to_firebase.ino
+++ test/upload_data_from_lg01_to_firebase/upload_data_from_lg01_to_firebase.ino
@@ -1,10 +1,13 @@
 #include <Process.h>
-#include <SPI.h>
 #include <Console.h>
+#include <SPI.h>
+//If you use Dragino IoT Mesh Firmware, uncomment below lines.
+//For product: LG01. 
+#define BAUDRATE 115200
 
 void setup()
 {
-  Bridge.begin();   // Initialize the Bridge
+  Bridge.begin(BAUDRATE);   // Initialize the Bridge
   Console.begin();
 }
 
@@ -12,9 +15,10 @@ void loop()
 {
   // Simulate Get Sensor value
   Console.println("===== Start =====");
-  int sensor = random(10, 20); 
+  int sensor = random(10, 20);
+  Console.println(sensor);
   Process p;
-  p.runShellCommand("curl -k -X POST https://lg01-ba3b9.firebaseio.com/temperature.json -d '{ \"value\" : " + String(sensor) + "}'");
+  p.runShellCommand("curl -k -X PUT https://lorawan-53a5b.firebaseio.com/temperature.json -d '{ \"value\" : " + String(sensor) + "}'");
   while(p.running());
   Console.println("===== End ====="); 
   delay(2000);                
