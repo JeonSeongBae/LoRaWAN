bd94a9cce79a9a2129a778b81c726ae14fffea65
JeonSeongBae <tjdqo6042@naver.com>
2018-02-19 04:46:21 +0900

    single update database

--- Application/app/src/main/java/kaiser/myapplication/MainActivity.java
+++ Application/app/src/main/java/kaiser/myapplication/MainActivity.java
@@ -1,17 +1,58 @@
 package kaiser.myapplication;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-    private DatabaseReference mDatabase;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    Context mContext;
+    EditText temperature;
+    EditText huminity;
+    Button update;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+        mContext = this;
+        temperature = findViewById(R.id.temperature);
+        huminity = findViewById(R.id.huminity);
+        update = findViewById(R.id.updateButton);
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference.child("users").child("name").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        HashMap<String, Double> name = (HashMap<String, Double>) dataSnapshot.getValue();
+                        temperature.setText(String.valueOf(name.get("temperature")));
+                        huminity.setText(String.valueOf(name.get("huminity")));
+                        Toast.makeText(mContext, String.valueOf(name.get("temperature"))+"\n"+String.valueOf(name.get("huminity")), Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
     }
 }
